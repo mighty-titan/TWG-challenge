b079db43b1ba0c5708b98a77b17b3af9d76452c7
Łukasz Nojman <l.nojman@emakina.pl>
2021-08-01 14:44:45 +0200

    feat: added bootstrap and sticky header

---
+++ components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="navbar sticky-top navbar-light bg-light px-4 d-flex justify-content-center">
+      <a className="navbar-brand" href="#">
+        klikrent
+      </a>
+    </nav>
+  );
+};
+
+export default Header;
